0f4538f1a494aac7c7b124763de483f4a59bbe7d
kzk00001 <kzk00001@hotmail.co.jp>
2021-07-22 15:36:25 +0900

    items/indexまで表示

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  # validates :product_name,       presence: true
+  # validates :text,  presence: true
+  # validates :price,     presence: true
+
+  # belongs_to :user
+  # has_one :purchase_record
+end
