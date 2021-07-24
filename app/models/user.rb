e4e1f92f7307bf364c9010c546861dd5e5f1e521
kzk00001 <kzk00001@hotmail.co.jp>
2021-07-24 15:09:11 +0900

    バリデーション・エラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,        presence: true
-         validates :first_name,      presence: true
-         validates :last_name,       presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana,  presence: true
+         validates :email,           presence: true, uniqueness: true
+         validates :password,        format: { with: /[a-z\d]{6,}/i, message: "is invalid. Include both letters and numbers" }
+         validates :nickname,        presence: true, uniqueness: true
+         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+         validates :first_name_kana, presence: true, format: { with: /\p{katakana}/, message: 'is invalid. Input full-width katakana characters' }
+         validates :last_name_kana,  presence: true, format: { with: /\p{katakana}/, message: 'is invalid. Input full-width katakana characters' }
          validates :birthdate,       presence: true
 end
