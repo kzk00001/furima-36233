959d3e719b92426797ae2ac90d2a4a0bc754009b
kzk00001 <kzk00001@hotmail.co.jp>
2021-07-26 15:45:44 +0900

    コードの追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,19 @@ class User < ApplicationRecord
 
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
-  validates :nickname,        presence: true, uniqueness: true
-  validates :first_name,      presence: true,
-                              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-  validates :last_name,       presence: true,
-                              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
-  validates :first_name_kana, presence: true,
-                              format: { with: /\p{katakana}/, message: 'is invalid. Input full-width katakana characters' }
-  validates :last_name_kana,  presence: true,
-                              format: { with: /\p{katakana}/, message: 'is invalid. Input full-width katakana characters' }
-  validates :birthdate,       presence: true
+
+  with_options presence: true do
+    validates :birthdate
+    with_options uniqueness: true do
+      validates :nickname
+    end
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\p{katakana}/, message: 'is invalid. Input full-width katakana characters' } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+  end
 end
