d96c6d3e88846501ccfc39fd13fba82e91314455
kzk00001 <kzk00001@hotmail.co.jp>
2021-07-27 16:59:15 +0900

    itemテーブルへの登録とバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,31 @@
 class Item < ApplicationRecord
-  # belongs_to :user
-  # has_one :purchase_record
+  belongs_to :user
   has_one_attached :image
+  # has_one :purchase_record
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :days_before_shipping
   belongs_to :shipping_method
+  belongs_to :prefecture
+  belongs_to :days_before_shipping
 
   with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :text
     # 「--」の時は保存できないようにする
     with_options numericality: { other_than: 0, message: "can't be blank" } do
-      validates :category
-      validates :condition
-      validates :days_before_shipping
-      validates :shipping_method
+      validates :category_id
+      validates :condition_id
+      validates :shipping_method_id
+      validates :prefecture_id
+      validates :days_before_shipping_id
+    end
+    with_options format: { with: /\A[0-9]+\z/} do
+      with_options numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} do
+        validates :price
+      end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_26_100422) do
+ActiveRecord::Schema.define(version: 2021_07_27_060112) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 2021_07_26_100422) do
     t.integer "shipping_method_id", null: false
     t.integer "prefecture_id", null: false
     t.integer "days_before_shipping_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -54,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_07_26_100422) do
     t.string "last_name", default: "", null: false
     t.string "first_name_kana", default: "", null: false
     t.string "last_name_kana", default: "", null: false
-    t.date "birthdate", default: "2021-07-26", null: false
+    t.date "birthdate", default: "2021-07-27", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_07_26_100422) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
