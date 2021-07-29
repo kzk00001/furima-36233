c42a758956cc675b36eef74caa64de29e366d5d6
kzk00001 <kzk00001@hotmail.co.jp>
2021-07-29 11:35:35 +0900

    コードの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :item_data, only: [:show, :edit, :update, :destroy]
-  before_action :owner?, only: [:edit, :update]
+  before_action :owner?, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: 'DESC')
